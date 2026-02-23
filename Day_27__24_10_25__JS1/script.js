94589617552bdd778a1c74de80653650c7fe1bee
Meet Chauhan <meetsc03@gmail.com>
2026-02-23 15:17:29 +0530

    Day27

---
+++ Day_27__24_10_25__JS1/script.js
@@ -0,0 +1,14 @@
+// Brandan Eich created js in just 15 days so there were many errors and bugs so to fix and fix it
+//  more versions came but the biggest version came was ES6 
+// but ES6 made the biggest change in js because it changed whole js 
+
+
+console.log("Heelo");
+console.error("thund");
+console.warn("heateddddd");
+
+alert("Hacking!!!!!");
+
+prompt("adddding");
+
+console.log("hakunamatata".slice(2,8));
